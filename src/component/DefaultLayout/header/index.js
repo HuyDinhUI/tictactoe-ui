9896f49969aab7b6d424698844fe160b6f8c876a
HuyDinhUI <146050567+HuyDinhUI@users.noreply.github.com>
2025-02-25 22:02:31 +0700

    add menu-user

--- src/component/DefaultLayout/header/index.js
+++ src/component/DefaultLayout/header/index.js
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import styles from "./header.module.css";
-const Header = ({ currenPage = "home" }) => {
+import Tippy from "@tippyjs/react/headless";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear, faRightFromBracket, faUser } from "@fortawesome/free-solid-svg-icons";
+const Header = ({ username = "" }) => {
   const logo2 = require("../../../assest/game (1).png");
 
   return (
@@ -17,9 +20,35 @@ const Header = ({ currenPage = "home" }) => {
           report
         </Link>
       </div>
-      <div className={styles.user}>
-        <p className={styles["user-name-short"]}>H</p>
-      </div>
+      <Tippy
+        placement="bottom-end"
+        trigger="click"
+        interactive
+        render={(attrs) => (
+          <div className={styles["menu-user"]} tabIndex="-1" {...attrs}>
+            <div className={styles["menu-user--container"]}>
+              <div className={styles["menu-item"]}>
+                <button className={styles["menu-btn"]}>
+                  <FontAwesomeIcon className={styles["menu-icon"]} icon={faUser}></FontAwesomeIcon>
+                  <p className={styles["menu-title"]}>View user</p>
+                </button>
+                <button className={styles["menu-btn"]}>
+                  <FontAwesomeIcon className={styles["menu-icon"]} icon={faGear}></FontAwesomeIcon>
+                  <p className={styles["menu-title"]}>Setting</p>
+                </button>
+                <button className={styles["menu-btn"]}>
+                  <FontAwesomeIcon className={styles["menu-icon"]} icon={faRightFromBracket}></FontAwesomeIcon>
+                  <p className={styles["menu-title"]}>Sign out</p>
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      >
+        <div className={styles.user}>
+          <p className={styles["user-name-short"]}>H</p>
+        </div>
+      </Tippy>
     </div>
   );
 };
